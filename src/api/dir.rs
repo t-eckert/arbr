d2f86e60b72fe798106b2c5e3d5b582d9a82c1ca
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-11-06 15:39:40 -0400

    Serve a map of files and directories from dir

--- src/api/dir.rs
+++ src/api/dir.rs
@@ -1,12 +1,12 @@
 use std::path::PathBuf;
 
-use rocket::{State, serde::json::Json};
+use rocket::{State, serde::{json::Json, Serialize}};
 
 use crate::Root;
 
 
 #[rocket::get("/dir/<path..>")]
-pub async fn dir(path: PathBuf, root: &State<Root>) -> Json<Vec<String>> {
+pub async fn dir(path: PathBuf, root: &State<Root>) -> Json<Dir> {
     let root_path = root.0.clone();
     let mut path = root_path.join(path);
 
@@ -14,13 +14,33 @@ pub async fn dir(path: PathBuf, root: &State<Root>) -> Json<Vec<String>> {
         path.pop();
     }
 
-    let mut files = Vec::new();
+    let mut dir = Dir{
+        files: Vec::new(),
+        markdown: Vec::new(),
+        directories: Vec::new(),
+    };
+
     for entry in path.read_dir().unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
         let name = path.file_name().unwrap().to_str().unwrap();
-        files.push(name.to_string());
+
+        if path.is_file() {
+            dir.files.push(name.to_string());
+            if name.ends_with(".md") {
+                dir.markdown.push(name.to_string());
+            }
+        } else if path.is_dir() && ! name.starts_with(".") {
+            dir.directories.push(name.to_string());
+        };
     }
 
-    Json(files)
+    Json(dir)
+}
+
+#[derive(Serialize)]
+pub struct Dir {
+    files: Vec<String>,
+    markdown: Vec<String>,
+    directories: Vec<String>,
 }
