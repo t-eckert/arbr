5d522c891f61488a8916e24c20f2210a50823eec
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-11-06 11:40:07 -0400

    Add /dir/<path..> (#2)

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate rocket;
 
 use clap::{crate_version, crate_authors, Parser};
 use comrak::{markdown_to_html, ComrakOptions};
-use rocket::{State, response::content};
+use rocket::{State, response::content, serde::json::Json};
 use std::{io, fs::read_to_string, path::PathBuf};
 
 #[derive(Parser)]
@@ -21,7 +21,7 @@ async fn main() {
 
     rocket::build()
         .manage(Root(root))
-        .mount("/api", rocket::routes![raw, html])
+        .mount("/api", rocket::routes![raw, html, dir])
         .launch()
         .await
         .expect("Failed to launch rocket");
@@ -55,4 +55,24 @@ async fn html(path: PathBuf, root: &State<Root>) -> Option<content::Html<String>
     let markdown = fetch_markdown(path).unwrap();
 
     Some(content::Html(markdown_to_html(&markdown[..], &ComrakOptions::default())))
+}
+
+#[rocket::get("/dir/<path..>")]
+async fn dir(path: PathBuf, root: &State<Root>) -> Json<Vec<String>> {
+    let root_path = root.0.clone();
+    let mut path = root_path.join(path);
+
+    if path.is_file() {
+        path.pop();
+    }
+
+    let mut files = Vec::new();
+    for entry in path.read_dir().unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        let name = path.file_name().unwrap().to_str().unwrap();
+        files.push(name.to_string());
+    }
+
+    Json(files)
 }
