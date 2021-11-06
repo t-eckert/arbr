2ec934d6458d4c109324adcacc2481c04324ae1f
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-11-06 11:20:15 -0400

    Allow user to pass in value for --root

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 extern crate rocket;
 
+use clap::{crate_version, crate_authors, Parser};
 use comrak::{markdown_to_html, ComrakOptions};
 use rocket::{State, response::content};
 use std::{io, fs::read_to_string, path::PathBuf};
+
+#[derive(Parser)]
+#[clap(version = crate_version!(), author = crate_authors!())]
+struct Opts {
+    #[clap(short, long, default_value = "./")]
+    root: String,
+}
 
 struct Root(PathBuf);
 
 #[rocket::main]
 async fn main() {
-    let root = PathBuf::from("/home/thomaseckert/Notebook/");
+    let opts = Opts::parse();
+    let root = PathBuf::from(opts.root);
 
     rocket::build()
         .manage(Root(root))
-        .mount("/api", rocket::routes![serve_raw, serve_html])
+        .mount("/api", rocket::routes![raw, html])
         .launch()
         .await
         .expect("Failed to launch rocket");
@@ -28,9 +37,10 @@ fn fetch_markdown(path: PathBuf) -> Result<String, io::Error> {
 }
 
 #[rocket::get("/raw/<path..>")]
-async fn serve_raw(path: PathBuf, root: &State<Root>) -> Option<content::Plain<String>> {
+async fn raw(path: PathBuf, root: &State<Root>) -> Option<content::Plain<String>> {
     let root_path = root.0.clone();
     let path = root_path.join(path);
+
     let markdown = fetch_markdown(path).unwrap();
 
     Some(content::Plain(markdown))
@@ -38,9 +48,10 @@ async fn serve_raw(path: PathBuf, root: &State<Root>) -> Option<content::Plain<S
 
 
 #[rocket::get("/html/<path..>")]
-async fn serve_html(path: PathBuf, root: &State<Root>) -> Option<content::Html<String>> {
+async fn html(path: PathBuf, root: &State<Root>) -> Option<content::Html<String>> {
     let root_path = root.0.clone();
     let path = root_path.join(path);
+
     let markdown = fetch_markdown(path).unwrap();
 
     Some(content::Html(markdown_to_html(&markdown[..], &ComrakOptions::default())))
