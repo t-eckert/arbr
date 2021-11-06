2e9f0a613b0000520650d7be3422a5027c42e1db
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-11-05 23:22:52 -0400

    Pass root as state

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,47 @@
 extern crate rocket;
 
 use comrak::{markdown_to_html, ComrakOptions};
-use rocket::fs::NamedFile;
-use rocket::response::content;
-use std::path::PathBuf;
-use std::fs::read_to_string;
+use rocket::{State, response::content};
+use std::{io, fs::read_to_string, path::PathBuf};
 
+struct Root(PathBuf);
 
 #[rocket::main]
 async fn main() {
+    let root = PathBuf::from("/home/thomaseckert/Notebook/");
+
     rocket::build()
+        .manage(Root(root))
         .mount("/api", rocket::routes![serve_raw, serve_html])
         .launch()
         .await
         .expect("Failed to launch rocket");
 }
 
-#[rocket::get("/raw/<path..>")]
-async fn serve_raw(path: PathBuf) -> Option<NamedFile> {
-
-    let root = PathBuf::from("/home/thomaseckert/Notebook/");
-    let mut path = root.join(path);
+fn fetch_markdown(path: PathBuf) -> Result<String, io::Error> {
+    let mut path = path;
     if path.is_dir() {
         path.push("README.md");
     }
 
-    NamedFile::open(path).await.ok()
+    read_to_string(path)
+}
+
+#[rocket::get("/raw/<path..>")]
+async fn serve_raw(path: PathBuf, root: &State<Root>) -> Option<content::Plain<String>> {
+    let root_path = root.0.clone();
+    let path = root_path.join(path);
+    let markdown = fetch_markdown(path).unwrap();
+
+    Some(content::Plain(markdown))
 }
 
 
 #[rocket::get("/html/<path..>")]
-async fn serve_html(path: PathBuf) -> Option<content::Html<String>> {
+async fn serve_html(path: PathBuf, root: &State<Root>) -> Option<content::Html<String>> {
+    let root_path = root.0.clone();
+    let path = root_path.join(path);
+    let markdown = fetch_markdown(path).unwrap();
 
-    let root = PathBuf::from("/home/thomaseckert/Notebook/");
-    let mut path = root.join(path);
-    if path.is_dir() {
-        path.push("README.md");
-    }
-
-    Some(content::Html(markdown_to_html(read_to_string(path).as_deref().unwrap_or(""), &ComrakOptions::default())))
+    Some(content::Html(markdown_to_html(&markdown[..], &ComrakOptions::default())))
 }
